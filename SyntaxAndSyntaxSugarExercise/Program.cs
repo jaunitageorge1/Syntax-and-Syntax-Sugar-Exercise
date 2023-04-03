23f3517e40de8b54bb70f089cb1363fe36099ba1
jaunitageorge1 <jaunitageorge@gmail.com>
2023-04-03 11:05:56 -0400

    edits

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             var answer = 4;
-            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine"
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
 
             Console.WriteLine(response);
 
